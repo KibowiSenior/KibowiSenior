7b8a022ff16f48b51fd7bd99fee00624eef58bfb
YourSenior <imkiwi07@yahoo.com>
2026-01-21 02:14:33 +0530

    Create hello.jsx

---
+++ hello.jsx
@@ -0,0 +1,11 @@
+export default () => (
+  <div style={{
+    height:"100vh",
+    display:"flex",
+    justifyContent:"center",
+    alignItems:"center",
+    fontSize:"32px"
+  }}>
+    Hello — still running, not dead.
+  </div>
+);
