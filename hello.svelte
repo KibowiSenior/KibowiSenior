879f56942213609755245da79a1b3eb8401e41f1
YourSenior <imkiwi07@yahoo.com>
2026-01-21 02:14:56 +0530

    Create hello.svelte

---
+++ hello.svelte
@@ -0,0 +1,11 @@
+<div class="center">Hello — still running, not dead.</div>
+
+<style>
+.center {
+  height:100vh;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  font-size:32px;
+}
+</style>
